2441850f05be73cc33306b4c40141b0d626e3742
kirirut <rutkkir@gmail.com>
2025-09-25 16:35:12 +0300

    add TimetablePO

---
+++ src/test/java/ui/PageObjects/TimetablePO.java
@@ -0,0 +1,45 @@
+package ui.PageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+import static ui.StepMethods.Driver.driver;
+import static ui.StepMethods.Driver.wait;
+
+public class TimetablePO {
+    public TimetablePO() {
+        switchToNewTab();
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(css = ".sc-3c1f9566-0.bJkzrz")
+    private List<WebElement> selectSeatsButtons;
+
+
+
+    private void switchToNewTab() {
+        List<String> tabs = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(tabs.size() - 1));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(driver -> !driver.getCurrentUrl().equals("data:,"));
+        System.out.println("Switched to tab URL: " + driver.getCurrentUrl());
+    }
+
+
+
+    public void selectFirstTrain() {
+        List<WebElement> buttons = wait.until(ExpectedConditions
+                .visibilityOfAllElementsLocatedBy(By.xpath("//button[contains(., 'Select Seats')]")));
+        WebElement firstButton = wait.until(ExpectedConditions.elementToBeClickable(buttons.get(0)));
+        firstButton.click();
+    }
+}
